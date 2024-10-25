4233b03469ca89b3bbf88202bab7c207f2d09a63
teresia wairimu <teresiawairimu06@gmail.com>
2024-10-25 17:11:29 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -30,6 +30,28 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs
+
+    Parameter
+    method: Callable
+        The method to be decorated
+
+    Returns
+    Callable
+        The decorated method with a call history
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+        return output
+    return wrapper
+
+
 class Cache:
     """A class used to interact with a Redis cache to store and retrieve data.
     Attributes
@@ -43,6 +65,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores given data in Redis, returns a radomly generated key
 
